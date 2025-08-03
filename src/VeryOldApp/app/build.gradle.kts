db193e1243eaf60db5dc63163133328619b68ee3
Nobuhiro Kuroiwa <nobubijinesu@gmail.com>
2025-08-03 14:53:43 +0900

    Add dependencies for exoplayer

--- src/VeryOldApp/app/build.gradle.kts
+++ src/VeryOldApp/app/build.gradle.kts
@@ -51,6 +51,24 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
 
+    // Json
+    implementation(libs.gson)
+
+    // Exoplayer
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.androidx.media3.player)
+    implementation(libs.androidx.media3.session)
+
+    // Unit Test
+    testImplementation(libs.hilt.android.testing)
+    testImplementation(libs.junit)
+    testImplementation(libs.androidx.core.testing)
+
+    // Android Unit Test
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.core.testing)
+
     // Hilt
     implementation(libs.androidx.hilt.navigation.compose)
     implementation(libs.hilt.android)
@@ -60,20 +78,4 @@ dependencies {
     testImplementation(libs.androidx.runner)
     androidTestImplementation(libs.hilt.android.testing)
     kapt(libs.hilt.android.compiler)
-
-    // Json
-    implementation(libs.gson)
-
-    // Unit Test
-    testImplementation(libs.hilt.android.testing)
-    testImplementation(libs.junit)
-    testImplementation(libs.androidx.core.testing)
-
-    // Android Unit Test
-    androidTestImplementation(libs.androidx.runner)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(libs.hilt.android.testing)
-    androidTestImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.core.testing)
-    androidTestImplementation("androidx.test:runner:1.5.0")
 }
