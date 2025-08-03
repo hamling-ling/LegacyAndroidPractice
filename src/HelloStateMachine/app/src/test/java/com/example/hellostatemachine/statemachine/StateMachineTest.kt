67eff274ef8dd4ce89a9f5811831cf33ac5179fc
Nobuhiro Kuroiwa <nobubijinesu@gmail.com>
2025-08-03 11:44:34 +0900

    update state machine unit test to adopt updated json format

--- src/HelloStateMachine/app/src/test/java/com/example/hellostatemachine/statemachine/StateMachineTest.kt
+++ src/HelloStateMachine/app/src/test/java/com/example/hellostatemachine/statemachine/StateMachineTest.kt
@@ -9,94 +9,81 @@ import org.junit.Test
 
 class StateMachineTest {
 
-    private lateinit var states: Map<String, State<MyActionParam>>
+    private lateinit var states: StateMachineDefinition<MyActionParam>
 
     private val jsonString = """
         {
-          "initial":
+          "states": [
             {
-              "name": "initial",
-              "transitions": {
-                "doSomething": 
+              "name": "${StateMachine.INITIAL_STATE_NAME}",
+              "transitionList": [
                 {
-                  "event": "doSomething",
+                  "eventName": "doSomething",
                   "nextStateName": "done",
                   "param": {
                     "param1": "my param1 for initial.doSomething",
                     "param2": "my param2 for initial.doSomething"
-                  },
-                  "action": null
+                  }
                 },
-                "doSomethingElse": 
                 {
-                  "event": "doSomethingElse",
+                  "eventName": "doSomethingElse",
                   "nextStateName": "done",
                   "param": {
                     "param1": "my param1 for initial.doSomethingElse",
                     "param2": "my param2 for initial.doSomethingElse"
-                  },
-                  "action": null
+                  }
                 }
-              }
+              ]
             },
-            "done":
             {
               "name": "done",
-              "transitions": {
-                "doSomething": 
+              "transitionList": [
                 {
-                  "event": "doSomething",
+                  "eventName": "doSomething",
                   "nextStateName": "done",
                   "param": {
                     "param1": "my param1 for done.doSomething",
                     "param2": "my param2 for done.doSomething"
-                  },
-                  "action": null
+                  }
                 },
-                "doSomethingElse": 
                 {
-                  "event": "doSomethingElse",
+                  "eventName": "doSomethingElse",
                   "nextStateName": "fail",
                   "param": {
                     "param1": "my param1 for done.doSomethingElse",
                     "param2": "my param2 for done.doSomethingElse"
-                  },
-                  "action": null
+                  }
                 }
-              }
+              ]
             },
-            "fail":
             {
               "name": "fail",
-              "transitions": {
-                "doSomething": 
+              "transitionList": [
                 {
-                  "event": "doSomething",
+                  "eventName": "doSomething",
                   "nextStateName": "done",
                   "param": {
                     "param1": "my param1 for fail.doSomething",
                     "param2": "my param2 for fail.doSomething"
-                  },
-                  "action": null
+                  }
                 },
-                "doSomethingElse": 
                 {
-                  "event": "doSomethingElse",
+                  "eventName": "doSomethingElse",
                   "nextStateName": "fail",
                   "param": {
                     "param1": "my param1 for fail.doSomethingElse",
                     "param2": "my param2 for fail.doSomethingElse"
-                  },
-                  "action": null
+                  }
                 }
-              }
+              ]
             }
+          ]
         }
     """.trimIndent()
 
     @Before
     fun setUp() {
-        states = Json.decodeFromString<Map<String, State<MyActionParam>>>(jsonString)
+        states = Json.decodeFromString<StateMachineDefinition<MyActionParam>>(jsonString)
 
         states["initial"]!!.transitions["doSomething"]!!.action = { param ->
             println("initial.doSomething called")
